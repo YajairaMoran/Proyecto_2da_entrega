bb9557929f85577316ee850504892095b0d7b1bf
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2024-04-28 14:15:25 -0600

    Add files via upload

---
+++ Inventario/src/inventario/Usuario.java
@@ -0,0 +1,77 @@
+package inventario;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author danie
+ */
+public class Usuario {
+    
+    private String mNombre;
+    private String mPass;
+    private String mCorreo;
+    //private String mRolUsuario;
+
+    /**
+     * @return the mNombre
+     */
+    public String getNombre() {
+        return mNombre;
+    }
+
+    /**
+     * @param mNombre the mNombre to set
+     */
+    public void setNombre(String mNombre) {
+        this.mNombre = mNombre;
+    }
+
+    /**
+     * @return the mPassword
+     */
+    public String getPassword() {
+        return mPass;
+    }
+
+    /**
+     * @param mPassword the mPassword to set
+     */
+    public void setPassword(String mPassword) {
+        this.mPass = mPassword;
+    }
+
+    /**
+     * @return the mCorreo
+     */
+    public String getCorreo() {
+        return mCorreo;
+    }
+
+    /**
+     * @param mCorreo the mCorreo to set
+     */
+    public void setCorreo(String mCorreo) {
+        this.mCorreo = mCorreo;
+    }
+
+    /**
+     * @return the mRolUsuario
+     */
+    /*public String getRolUsuario() {
+        return mRolUsuario;
+    }*/
+
+    /**
+     * @param mRolUsuario the mRolUsuario to set
+     */
+    /*public void setRolUsuario(String mRolUsuario) {
+        this.mRolUsuario = mRolUsuario;
+    }*/
+    
+    
+    
+}
